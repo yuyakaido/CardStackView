20762e76399233ce407043fbb19160b99d9476f0
Yuya Kaido <yuyakaido@gmail.com>
2017-08-12 00:19:05 +0900

    Added feature to set StackFrom from XML

--- cardstackview/src/main/java/com/yuyakaido/android/cardstackview/CardStackView.java
+++ cardstackview/src/main/java/com/yuyakaido/android/cardstackview/CardStackView.java
@@ -83,6 +83,7 @@ public class CardStackView extends FrameLayout {
         TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.CardStackView);
         setVisibleCount(array.getInt(R.styleable.CardStackView_visibleCardCount, option.visibleCount));
         setSwipeThreshold(array.getInt(R.styleable.CardStackView_swipeThreshold, (int) option.swipeThreshold));
+        setStackFrom(StackFrom.values()[array.getInt(R.styleable.CardStackView_stackFrom, option.stackFrom.ordinal())]);
         array.recycle();
     }
 
--- cardstackview/src/main/java/com/yuyakaido/android/cardstackview/StackFrom.java
+++ cardstackview/src/main/java/com/yuyakaido/android/cardstackview/StackFrom.java
@@ -1,22 +1,6 @@
 package com.yuyakaido.android.cardstackview;
 
 public enum StackFrom {
-    Bottom("bottom"), Top("top");
-
+    Bottom, Top;
     public static final StackFrom DEFAULT = Bottom;
-
-    private String value;
-
-    StackFrom(String value) {
-        this.value = value;
-    }
-
-    public static StackFrom from(String value) {
-        for (StackFrom direction : values()) {
-            if (direction.value.equals(value)) {
-                return direction;
-            }
-        }
-        return DEFAULT;
-    }
 }
