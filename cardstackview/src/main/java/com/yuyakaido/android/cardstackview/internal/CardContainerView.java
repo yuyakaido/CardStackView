900e61a5689ec164ca7dfabf5252cd8333267e59
Gonçalo Correia <Goncalo.Correia@net-a-porter.com>
2018-01-17 17:54:46 +0000

    Fixing card position when swiping to the right

--- cardstackview/src/main/java/com/yuyakaido/android/cardstackview/internal/CardContainerView.java
+++ cardstackview/src/main/java/com/yuyakaido/android/cardstackview/internal/CardContainerView.java
@@ -178,10 +178,17 @@ public class CardContainerView extends FrameLayout {
     }
 
     private void updateTranslation(MotionEvent event) {
-        float translationX = viewOriginX + event.getRawX() - motionOriginX;
-        if (getDirection(event.getRawX(), event.getRawY()) == Right) {
-            //translationX -= getWidth();
-        }
+        Log.d("ASDADS", "direction: " + getDirection(event.getRawX(), event.getRawY()));
+        Log.d("ASDASD", "rawX: " + event.getRawX());
+        Log.d("ASDASD", "viewOriginX: " + viewOriginX);
+        Log.d("ASDASD", "motionOrigin: " + motionOriginX);
+        float translationX = 0f;
+
+        translationX = viewOriginX + event.getRawX() - motionOriginX;
+        if (translationX > 0) translationX = viewOriginX - (getWidth() / 2) + (event.getRawX() - motionOriginX);
+
+        Log.d("ASDASD", "width: " + getWidth());
+        Log.d("ASDASD", "translationX: " + translationX);
         ViewCompat.setTranslationX(this, translationX);
         ViewCompat.setTranslationY(this, viewOriginY);
     }
