92d1c9e6f0947cc1ec517a470f33f50fd8b429b5
Luong Vo <luong.vo@buddify.io>
2018-03-22 11:24:04 +0700

    [b] don't swipe card if swipeDirection is updated to lock this direction when the card is moving

--- cardstackview/src/main/java/com/yuyakaido/android/cardstackview/CardStackView.java
+++ cardstackview/src/main/java/com/yuyakaido/android/cardstackview/CardStackView.java
@@ -328,6 +328,11 @@ public class CardStackView extends FrameLayout {
     }
 
     private void executePostSwipeTask(Point point, SwipeDirection direction) {
+        // don't swipe card if swipeDirection is updated to lock this direction when the card is moving
+        if (!option.swipeDirection.contains(direction)) {
+            return;
+        }
+
         reorderForSwipe();
 
         state.lastPoint = point;
