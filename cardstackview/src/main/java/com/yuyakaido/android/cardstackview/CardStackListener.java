b6a6f9096c232b4cd63e214d301b38ad2a578e7b
Barryrowe <barryrowe@gmail.com>
2019-10-28 10:17:25 -0400

    Provide swiped position to onCardSwiped event
    Whenever a card is swiped, this also captures the position
    of the swiped card and emits it to the event. This allows
    us to trust the position at the event handler, and not have
    to inspect the cardLayoutManager directly. This is helpful
    in the case where our event may trigger another runnable or
    otherwise may not be able to trust that the layoutManager
    has not already calculated another topPosition.

--- cardstackview/src/main/java/com/yuyakaido/android/cardstackview/CardStackListener.java
+++ cardstackview/src/main/java/com/yuyakaido/android/cardstackview/CardStackListener.java
@@ -4,7 +4,7 @@ import android.view.View;
 
 public interface CardStackListener {
     void onCardDragging(Direction direction, float ratio);
-    void onCardSwiped(Direction direction);
+    void onCardSwiped(Direction direction, int swipedPosition);
     void onCardRewound();
     void onCardCanceled();
     void onCardAppeared(View view, int position);
@@ -14,7 +14,7 @@ public interface CardStackListener {
         @Override
         public void onCardDragging(Direction direction, float ratio) {}
         @Override
-        public void onCardSwiped(Direction direction) {}
+        public void onCardSwiped(Direction direction, int swipedPosition) {}
         @Override
         public void onCardRewound() {}
         @Override
