9757d04655df44ab52c24632b574d7fa0144c512
godslew <maousnake00@live.jp>
2022-09-29 17:30:43 +0900

    WIP use generics

--- cardstackview/src/main/java/com/yuyakaido/android/cardstackview/compose/CardStackView.kt
+++ cardstackview/src/main/java/com/yuyakaido/android/cardstackview/compose/CardStackView.kt
@@ -4,38 +4,29 @@ import android.util.Log
 import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
 import kotlinx.coroutines.launch
 import kotlin.math.min
 
-data class Item(
-    val id: Int,
-    val color: Color
-)
-
 @Composable
-fun CardStackView(
-    items: List<Item>
+inline fun <T> CardStackView(
+    items: List<T>,
+    modifier: Modifier = Modifier,
+    crossinline content: @Composable (T) -> Unit
 ) {
     val cardWidthDp = 300.dp
     val cardHeightDp = 400.dp
@@ -47,7 +38,7 @@ fun CardStackView(
     val scales = remember { List(visibleSize) { Animatable(1f - it * 0.05f) } }
     val scope = rememberCoroutineScope()
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         repeat(visibleSize) { index ->
@@ -73,11 +64,23 @@ fun CardStackView(
                             },
                             onDragEnd = {
                                 Log.d("CardStackView", "onDragEnd")
-                                Log.d("CardStackView", "TranslationX: ${translationX.velocityVector.value}")
-                                Log.d("CardStackView", "TranslationY: ${translationY.velocityVector.value}")
-                                val cardDistance = Offset(cardWidthDp.toPx(), cardHeightDp.toPx()).getDistance() / 2
+                                Log.d(
+                                    "CardStackView",
+                                    "TranslationX: ${translationX.velocityVector.value}"
+                                )
+                                Log.d(
+                                    "CardStackView",
+                                    "TranslationY: ${translationY.velocityVector.value}"
+                                )
+                                val cardDistance = Offset(
+                                    cardWidthDp.toPx(),
+                                    cardHeightDp.toPx()
+                                ).getDistance() / 2
                                 Log.d("CardStackView", "CardDistance: $cardDistance")
-                                val translationDistance = Offset(translationX.targetValue, translationY.targetValue).getDistance()
+                                val translationDistance = Offset(
+                                    translationX.targetValue,
+                                    translationY.targetValue
+                                ).getDistance()
                                 Log.d("CardStackView", "TranslationDistance: $translationDistance")
                                 val percentage = min(1f, translationDistance / cardDistance)
                                 if (percentage < 0.5f) {
@@ -143,8 +146,14 @@ fun CardStackView(
                                     translationY.animateTo(translationY.targetValue + dragAmount.y)
                                 }
                                 scope.launch {
-                                    val cardDistance = Offset(cardWidthDp.toPx(), cardHeightDp.toPx()).getDistance() / 2
-                                    val translationDistance = Offset(translationX.targetValue, translationY.targetValue).getDistance()
+                                    val cardDistance = Offset(
+                                        cardWidthDp.toPx(),
+                                        cardHeightDp.toPx()
+                                    ).getDistance() / 2
+                                    val translationDistance = Offset(
+                                        translationX.targetValue,
+                                        translationY.targetValue
+                                    ).getDistance()
                                     val percentage = min(1f, translationDistance / cardDistance)
                                     val proportion = 1f - percentage
                                     val targetScale = 1f - proportion * 0.05f
@@ -156,15 +165,10 @@ fun CardStackView(
                     .size(
                         width = cardWidthDp,
                         height = cardHeightDp
-                    )
-                    .clip(RoundedCornerShape(16.dp))
-                    .background(item.color),
+                    ),
                 contentAlignment = Alignment.Center
             ) {
-                Text(
-                    text = item.id.toString(),
-                    fontSize = 80.sp
-                )
+                content(item)
             }
         }
     }
--- sample/src/main/java/com/yuyakaido/android/cardstackview/sample/ComposeActivity.kt
+++ sample/src/main/java/com/yuyakaido/android/cardstackview/sample/ComposeActivity.kt
@@ -3,35 +3,151 @@ package com.yuyakaido.android.cardstackview.sample
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.collectAsState
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.yuyakaido.android.cardstackview.compose.CardStackView
-import com.yuyakaido.android.cardstackview.compose.Item
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlin.random.Random
 
 class ComposeActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val random = Random(System.currentTimeMillis())
+        val spots = createSpots()
         setContent {
-            val items = MutableStateFlow(
-                List(100) {
-                    Item(
-                        id = it,
-                        color = Color(
-                            red = random.nextInt(),
-                            green = random.nextInt(),
-                            blue = random.nextInt()
-                        )
-                    )
-                }
-            )
             CardStackView(
-                items = items.collectAsState().value
-            )
+                items = spots,
+            ) {
+                Spot(
+                    spot = it,
+                    modifier = Modifier.fillMaxSize()
+                )
+            }
         }
     }
 
+    private fun createSpots(): List<Spot> {
+        val spots = ArrayList<Spot>()
+        spots.add(
+            Spot(
+                name = "Yasaka Shrine",
+                city = "Kyoto",
+                url = "https://source.unsplash.com/Xq1ntWruZQI/600x800"
+            )
+        )
+        spots.add(
+            Spot(
+                name = "Fushimi Inari Shrine",
+                city = "Kyoto",
+                url = "https://source.unsplash.com/NYyCqdBOKwc/600x800"
+            )
+        )
+        spots.add(
+            Spot(
+                name = "Bamboo Forest",
+                city = "Kyoto",
+                url = "https://source.unsplash.com/buF62ewDLcQ/600x800"
+            )
+        )
+        spots.add(
+            Spot(
+                name = "Brooklyn Bridge",
+                city = "New York",
+                url = "https://source.unsplash.com/THozNzxEP3g/600x800"
+            )
+        )
+        spots.add(
+            Spot(
+                name = "Empire State Building",
+                city = "New York",
+                url = "https://source.unsplash.com/USrZRcRS2Lw/600x800"
+            )
+        )
+        spots.add(
+            Spot(
+                name = "The statue of Liberty",
+                city = "New York",
+                url = "https://source.unsplash.com/PeFk7fzxTdk/600x800"
+            )
+        )
+        spots.add(
+            Spot(
+                name = "Louvre Museum",
+                city = "Paris",
+                url = "https://source.unsplash.com/LrMWHKqilUw/600x800"
+            )
+        )
+        spots.add(
+            Spot(
+                name = "Eiffel Tower",
+                city = "Paris",
+                url = "https://source.unsplash.com/HN-5Z6AmxrM/600x800"
+            )
+        )
+        spots.add(
+            Spot(
+                name = "Big Ben",
+                city = "London",
+                url = "https://source.unsplash.com/CdVAUADdqEc/600x800"
+            )
+        )
+        spots.add(
+            Spot(
+                name = "Great Wall of China",
+                city = "China",
+                url = "https://source.unsplash.com/AWh9C-QjhE4/600x800"
+            )
+        )
+        return spots
+    }
+}
+
+@Composable
+private fun Spot(
+    modifier: Modifier = Modifier,
+    spot: Spot
+) {
+    Box(modifier = modifier
+        .size(500.dp)
+        .background(color = Color.Black)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+                .align(Alignment.BottomStart)
+        ) {
+            Text(
+                text = spot.name,
+                fontSize = 26.sp,
+                color = Color.White,
+                fontWeight = FontWeight.W700,
+            )
+            Text(
+                text = spot.city,
+                fontSize = 20.sp,
+                color = Color.White,
+                fontWeight = FontWeight.W700,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun PreviewSpot() {
+    Spot(
+        spot = Spot(
+            name = "Yasaka Shrine",
+            city = "Kyoto",
+            url = "https://source.unsplash.com/Xq1ntWruZQI/600x800"
+        ),
+        modifier = Modifier.fillMaxSize(),
+    )
 }
