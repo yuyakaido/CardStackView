a6c8eb1769605a452dba69057feca0abee05b1f3
godslew <maousnake00@live.jp>
2022-10-05 18:53:18 +0900

    add stack

--- cardstackview/src/main/java/com/yuyakaido/android/cardstackview/compose/CardStackView.kt
+++ cardstackview/src/main/java/com/yuyakaido/android/cardstackview/compose/CardStackView.kt
@@ -1,13 +1,11 @@
 package com.yuyakaido.android.cardstackview.compose
 
 import android.util.Log
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.key
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
@@ -19,24 +17,31 @@ fun <T> CardStackView(
     items: List<T>,
     modifier: Modifier = Modifier,
     controller: CardStackViewControllerType<T> = rememberCardStackViewController(items),
+    stackCount: Int = 3,
+    paddingBetweenCards: Float = 20F,
     content: @Composable (T) -> Unit
 ) {
+    val visibleContents = items.filter {
+        !controller.currentCardController(it).isCardSwiped()
+    }.take(stackCount)
+
     Box(
         modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        val visibleSize = items.size
+        val visibleSize = visibleContents.size
         val zIndexes = remember { List(visibleSize) { mutableStateOf(visibleSize - it - 1) } }
-        items.forEachIndexed { index, item ->
+        visibleContents.forEachIndexed { index, item ->
             val zIndex = zIndexes[index].value
             val cardController = controller.currentCardController(item)
             key(item, cardController) {
+                val paddingTop by animateFloatAsState(targetValue = (zIndex * paddingBetweenCards))
                 Box(
                     modifier = modifier
                         .zIndex(zIndex.toFloat())
                         .graphicsLayer(
                             translationX = cardController.cardX,
-                            translationY = cardController.cardY,
+                            translationY = cardController.cardY + paddingTop,
                             rotationZ = cardController.rotation,
                         )
                         .pointerInput(Unit) {
--- sample/src/main/java/com/yuyakaido/android/cardstackview/sample/ComposeActivity.kt
+++ sample/src/main/java/com/yuyakaido/android/cardstackview/sample/ComposeActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -23,6 +22,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -245,11 +245,11 @@ private fun Spot(
 ) {
     Box(
         modifier = modifier
-            .background(color = Color.Black)
     ) {
         AsyncImage(
             model = spot.url,
             contentDescription = null,
+            contentScale = ContentScale.Crop,
             modifier = Modifier
                 .fillMaxWidth()
                 .aspectRatio(0.6F)
