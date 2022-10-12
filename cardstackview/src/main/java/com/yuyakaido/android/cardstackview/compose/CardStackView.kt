5b7d6f2ccadf4073d106edb8a6213b6674076842
godslew <maousnake00@live.jp>
2022-10-12 15:29:32 +0900

    fix

--- cardstackview/src/main/java/com/yuyakaido/android/cardstackview/compose/CardStackView.kt
+++ cardstackview/src/main/java/com/yuyakaido/android/cardstackview/compose/CardStackView.kt
@@ -4,7 +4,10 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -35,9 +38,8 @@ fun <T> CardStackView(
         contentAlignment = Alignment.Center
     ) {
         val visibleSize = visibleContents.size
-        val zIndexes = remember { List(visibleSize) { mutableStateOf(visibleSize - it - 1) } }
         visibleContents.forEachIndexed { index, item ->
-            val zIndex = zIndexes[index].value
+            val zIndex = visibleSize - index - 1
             val cardController = controller.currentCardController(item)
             key(item, cardController) {
                 val padding = PaddingBetweenCards.get(config.translationInterval, config.stackFrom)
